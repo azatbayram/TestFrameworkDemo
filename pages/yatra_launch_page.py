1763a0c7cfc7230e09ca609a16cc53aa7c703d94
azatbayram <bayramov.azat.tkm@gmail.com>
2023-02-17 01:37:17 +0300

    first test case

--- pages/yatra_launch_page.py
+++ pages/yatra_launch_page.py
@@ -3,8 +3,9 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 class LaunchPage():
-    def __init__(self, driver):
+    def __init__(self, driver, wait):
         self.driver=driver
+        self.wait=wait
 
     def departFrom(self, departLocation):
         depart_from = self.driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_city']")
--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -1,10 +1,7 @@
-import time
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-
-
+from pages.yatra_launch_page import LaunchPage
 
 @pytest.mark.usefixtures("setup")
 class TestSearchFlights():
@@ -14,3 +11,9 @@ class TestSearchFlights():
         # Providing going to location
         #To resolve sync issue
         #Click on search button
+        launchpage=LaunchPage(self.driver, self.wait)
+        launchpage.departFrom("New Delhi")
+        launchpage.goingTo("New York")
+        launchpage.selectDate("19/02/2023")
+        launchpage.clickSearch()
+
