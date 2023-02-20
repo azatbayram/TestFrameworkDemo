04ad88278f06aaa4fcdda46948df83b180c25861
azatbayram <bayramov.azat.tkm@gmail.com>
2023-02-21 01:34:45 +0300

    last version of launch page

--- pages/yatra_launch_page.py
+++ pages/yatra_launch_page.py
@@ -13,7 +13,7 @@ class LaunchPage(BaseDriver):
     #Locators
     DEPART_FROM_FIELD = "//input[@id='BE_flight_origin_city']"
     GOING_TO_FIELD = "//input[@id='BE_flight_arrival_city']"
-    GOING_TO_RESULT_LIST = "//div[@class='viewport']//div[1]/li"
+    GOING_TO_RESULT_LIST = "//div[@class='viewport']//div/li"
     SELECT_DATE_FIELD = "//input[@id='BE_flight_origin_date']"
     ALL_DATES = "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']"
     SEARCH_BUTTON = "//input[@value='Search Flights']"
@@ -25,7 +25,7 @@ class LaunchPage(BaseDriver):
         return self.wait_until_element_is_clickable(By.XPATH, self.GOING_TO_FIELD)
 
     def getGoingToResults(self):
-        return self.wait_for_presence_of_all_elements(By.XPATH, self.GOING_TO_RESULT_LIST)
+        return self.driver.find_elements(By.XPATH, self.GOING_TO_RESULT_LIST)
 
     def getDepatureDateField(self):
         return self.wait_until_element_is_clickable(By.XPATH, self.SELECT_DATE_FIELD)
@@ -38,19 +38,19 @@ class LaunchPage(BaseDriver):
 
     def enterDepartFromLocation(self, departLocation):
         self.getDepartFromField().click()
+        time.sleep(2)
         self.getDepartFromField().send_keys(departLocation)
+        time.sleep(2)
         self.getDepartFromField().send_keys(Keys.ENTER)
 
     def enterGoingToLocation(self, goingtolocation):
         self.getGoingToField().click()
-        self.log.info("Clicked on going to")
         time.sleep(2)
         self.getGoingToField().send_keys(goingtolocation)
-        self.log.info("Typed text into going to field successfully")
         time.sleep(2)
         search_results = self.getGoingToResults()
         for results in search_results:
-            if goingtolocation in results.text:
+            if "New York (JFK)" in results.text:
                 results.click()
                 break
 
@@ -71,5 +71,46 @@ class LaunchPage(BaseDriver):
         self.enterGoingToLocation(goingtolocation)
         self.enterDepartureDate(departuredate)
         self.clickSearchFlightsButton()
+
+   # def departFrom(self, departLocation):
+    #    depart_from = self.driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_city']")
+    #    depart_from.click()
+    #    time.sleep(2)
+    #    depart_from.clear()
+    #    depart_from.send_keys(departLocation)
+    #    depart_from.send_keys(Keys.ENTER)
+    #    time.sleep(3)
+
+    def goingTo(self, goingtoLocation):
+        going_to = self.driver.find_element(By.XPATH, "//input[@id='BE_flight_arrival_city']")
+        going_to.click()
+        time.sleep(2)
+        going_to.send_keys(goingtoLocation)
+
+        result_list = self.driver.find_elements(By.XPATH, "//div[@class='viewport']//div/li")
+
+        for result in result_list:
+            # print(result.text)
+            if "New York (JFK)" in result.text:
+                result.click()
+                break
+
+    def selectDate(self, departureDate):
+        depart_date = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='BE_flight_origin_date']")))
+        depart_date.click()
+        self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@id='monthWrapper']//tbody//td[@class!='inActiveTD']")))
+        date_list = self.driver.find_element(By.XPATH, "//input[@id='BE_flight_origin_date']").find_elements(By.XPATH,"//div[@id='monthWrapper']//tbody//td")
+
+        # Select departure date
+        for date in date_list:
+            if date.get_attribute("data-date") == departureDate:
+                date.click()
+                break
+
+    def clickSearch(self):
+        search_button = self.driver.find_element(By.XPATH, "//input[@value='Search Flights']")
+        search_button.click()
+
 
 
--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -7,18 +7,8 @@ from pages.search_flights_results_page import SearchFlightResultsPage
 @pytest.mark.usefixtures("setup")
 class TestSearchFlights():
     def test_seach_fights(self):
-        #Launch browser and navigate to website
-        #Providing going from location
-        # Providing going to location
-        #To resolve sync issue
-        #Click on search button
+
         launchpage=LaunchPage(self.driver, self.wait)
-        #launchpage.departFrom("New Delhi")
-        launchpage.enterDepartFromLocation("New Delhi")
-        launchpage.goingTo("New York")
-        launchpage.selectDate("21/02/2023")
-        launchpage.clickSearch()
-        launchpage.scrollPage()
-        #searchpage=SearchFlightResultsPage(self.driver, self.wait)
-        #searchpage.filter_flights_by_stop("1 Stop")
+        launchpage.searchFlights("New Delhi", "New York", "22/02/2023")
+
 
