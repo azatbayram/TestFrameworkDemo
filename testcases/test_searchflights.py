33041412ba5fcea329a0df053123ee1f44c6c8a5
azatbayram <bayramov.azat.tkm@gmail.com>
2023-02-21 03:29:06 +0300

    add softest

--- testcases/test_searchflights.py
+++ testcases/test_searchflights.py
@@ -1,11 +1,12 @@
 import pytest
+import softest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from pages.yatra_launch_page import LaunchPage
 from pages.search_flights_results_page import SearchFlightResultsPage
 
 @pytest.mark.usefixtures("setup")
-class TestSearchFlights():
+class TestSearchFlights(softest.TestCase):
     @pytest.fixture(autouse=True)
     def class_setup(self):
         self.launchpage=LaunchPage(self.driver, self.wait)
